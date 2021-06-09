24c17940a4b6298f35e736d4559a4ad6c12e0c30
Grzegorz Płonka <tradecomp.pl@gmail.com>
2021-06-09 19:03:01 +0200

    Adding code done at page 36 of book Pro Asp.net core3 writen by Adam Freeman

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,18 @@ namespace PartyInvites.Controllers
         {
             return View();
         }
+       
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
